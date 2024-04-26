fe1617760d4cf6fce702b240fab09986531fe0ca
Eduardo Hernandez F <eduardohf@ciencias.unam.mx>
2024-04-26 09:34:49 -0600

    Servicio inicioSesion Funcional

--- app.py
+++ app.py
@@ -2,15 +2,19 @@ from flask import Flask, redirect, render_template, url_for, request, flash, ses
 
 from controller.catalogue import catalogue
 from modelo.usuario import Usuarios
+from modelo import db
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://ferfong:Develooper123!@localhost:3306/ing_soft'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://ferfong:Develooper123!@localhost:3306/merkApp'
 app.config.from_mapping(
     SECRET_KEY='dev'
 )
 
+db.init_app(app)
+
+
 @app.route('/')
-def hello_world():
-    print("weaos")
+def inicioSesion():
     return redirect(url_for('login'))
 
 
@@ -25,19 +29,12 @@ def login():
     passwd = request.form.get('password')
     
     for registro in Usuarios.query.all():
-            id += 1
-            if name == registro.nombre and passwd == registro.password:
-                flash("ERROR: Pon otro correo!")
-            else:
+            if name == registro.username and passwd == registro.password:
                 session['user_id'] = name #definición de cookie de sesión.
                 return render_template('login.html', user=name)
-                #break
-    
+            else:
+                flash("ERROR: nombre de usuario o contraseña incorrectos")
 
-    # if name == 'ferfong' and passwd == 'Developer123!': #Ustedes van a tener que cambiar esto, por una validación con la DB.
-    #     session['user_id'] = name #definición de cookie de sesión.
-    #     return render_template('login.html', user=name)
-    # flash('Invalid username or password')
     return redirect(url_for('login'))
 
 
@@ -49,52 +46,3 @@ def logout():
 
 if __name__ == '__main__':
     app.run()
-
-# -----------------
-# from flask import Flask, render_template
-
-# from modelo import db
-# # from contollers.MenuControlador import menu_blueprint
-# # from contollers.UsuarioControlador import usuario_blueprint
-# # from contollers.PeliculaControlador import pelicula_blueprint
-# # from contollers.RentarControlador import rentar_blueprint
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:XUHVFImTtBMFHrDjhqcAlMOXLgLhPCAD@roundhouse.proxy.rlwy.net:29472/railway'
-# app.config.from_mapping(
-#     SECRET_KEY='dev'
-# )
-
-
-
-# if __name__ == '__main__':
-#     app.run()
-
-# ----------------------------------{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}
-
-
-# from flask import Flask, render_template
-
-# from modelo import db
-# # from contollers.MenuControlador import menu_blueprint
-# # from contollers.UsuarioControlador import usuario_blueprint
-# # from contollers.PeliculaControlador import pelicula_blueprint
-# # from contollers.RentarControlador import rentar_blueprint
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:XUHVFImTtBMFHrDjhqcAlMOXLgLhPCAD@roundhouse.proxy.rlwy.net:29472/railway'
-# app.config.from_mapping(
-#     SECRET_KEY='dev'
-# )
-# db.init_app(app)
-# # app.register_blueprint(menu_blueprint)
-# # app.register_blueprint(usuario_blueprint)
-# # app.register_blueprint(pelicula_blueprint)
-# # app.register_blueprint(rentar_blueprint)
-
-# @app.route('/')
-# def hello_world():  # put application's code here
-#     return render_template('index.html')
-
-# if __name__ == '__main__':
-#     app.run()
--- modelo/usuario.py
+++ modelo/usuario.py
@@ -1,11 +1,12 @@
 from sqlalchemy import Column, Integer, String, Boolean, BLOB
 from modelo  import db
+from flask import Flask, redirect, render_template, url_for, request, flash, session, Blueprint
 
 class Usuarios(db.Model):
 
     __tablename__ = 'usuario'
-    idUsuario = Column(Integer, nullable=False, primary_key=True)
-    nombre = Column(String(200), nullable=False)
+    user_id = Column(Integer, nullable=False, primary_key=True)
+    username = Column(String(200), nullable=False)
     # apPat = Column(String(200), nullable=False)
     # apMat = Column(String(200))
     password = Column(String(64), nullable=False)
@@ -20,6 +21,25 @@ class Usuarios(db.Model):
         self.password = password
         # self.email = email
         # self.rol = rol
+        
+    #Sugerencia, después poner las funciones en los modelos de tal manera que en app.py no tengamos eso y solo lo mandemos a llamar
+    def sesion(self, nombre, password):
+        if session.get('user_id') != None:
+            return render_template('login.html', user=session['user_id'])
+        if request.method == 'GET':
+            return render_template('index.html')
+        
+        name = request.form.get('username')
+        passwd = request.form.get('password')
+        
+        for registro in Usuarios.query.all():
+                id += 1
+                if name == registro.nombre and passwd == registro.password:
+                    flash("ERROR: Pon otro correo!")
+                else:
+                    session['user_id'] = name #definición de cookie de sesión.
+                    return render_template('login.html', user=name)
+        return redirect(url_for('login'))
 
     def __str__(self):
         return f'Nombre del usuario: {self.nombre} {self.apPat} {self.apMat}\n - Id: {self.idUsuario}\n - Correo: {self.email}\n - Contraseña: {self.password}\n - Rol: {self.superUser}\n'
