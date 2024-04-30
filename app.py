190244ed4f0bae1c0e551c02471e4b667e965ad3
JaviMQ28 <javier_manceraq@ciencias.unam.mx>
2024-04-30 15:10:24 -0600

    Flask final

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from flask import Flask, redirect, render_template, url_for, request, flash, session
+# https://stackoverflow.com/questions/73309491/port-xxxx-is-in-use-by-another-program-either-identify-and-stop-that-program-o
+# https://stackoverflow.com/questions/68012921/i-get-options-method-instead-of-post-in-flask
+# https://stackoverflow.com/questions/10434599/get-the-data-received-in-a-flask-request
+
+import json
+
+from flask import Flask, redirect, render_template, url_for, request, flash, session, Response
+from flask_cors import CORS
 
 from alchemyClasses import db
 from controller.catalogue import catalogue
@@ -11,40 +18,55 @@ app.register_blueprint(usuario_blueprint)
 app.config['SECRET_KEY'] = 'dev'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://ing:Developer123!@localhost:3306/usuario'
 db.init_app(app)
+CORS(app)
 
 @app.route('/')
 def hello_world():
     return redirect(url_for('login'))
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():    
+@app.route('/login', methods=['GET', 'POST', 'OPTIONS'])
+def login(): 
+    #print(f'\n{request.method}\n')    
+    if request.method == 'OPTIONS':           
+        res = Response()        
+        res.headers['X-Content-Type-Options'] = '*'
+        return res
     if session.get('user_id') != None:
         return render_template('login.html', user=session['user_id'])    
     if request.method == 'GET':        
         return render_template('index.html')
     boton = request.form.get('registrar')
-    print(boton)
+    #print(boton)
     if boton == 'registrar':
         return redirect(url_for('usuario.registrar_usuario'))
-    nombre = request.form.get('username')
-    contrasenia = request.form.get('password')
-    print(nombre)
-    print(contrasenia)
-    id = 1
-    for registro in Usuario.query.all():
-        id += 1
-        if nombre == registro.nombre:
-            flash("ERROR: Pon otro nombre!")
-    usuario = Usuario(id, nombre, contrasenia)
-    db.session.add(usuario)        
-    db.session.commit()        
-    encontrado = Usuario.query.filter(Usuario.nombre == nombre, Usuario.contrasenia == contrasenia).first()
-    if encontrado != None: #Ustedes van a tener que cambiar esto, por una validación con la DB.
-        session['user_id'] = nombre #definición de cookie de sesión.
-        return render_template('login.html', user=nombre)                
-    flash('Invalid username or password')
-    return redirect(url_for('login'))
+    if request.method == 'POST':   
+        #print(request.json)          
+        #print()
+        #nombre = request.form.get('username')
+        #contrasenia = request.form.get('password')
+                
+        nombre = request.json['nombre']
+        contrasenia = request.json['contrasenia']        
+        #print(f'\n{nombre}\n')
+        #print(contrasenia)
+        id = 1
+        for registro in Usuario.query.all():
+            id += 1
+            if nombre == registro.nombre:
+                flash("ERROR: Pon otro nombre!")
+        usuario = Usuario(id, nombre, contrasenia)
+        encontrado = Usuario.query.filter(Usuario.nombre == nombre, Usuario.contrasenia == contrasenia).first()
+        db.session.add(usuario)        
+        db.session.commit()                
+        print(f'\n{encontrado}\n')    
+        if encontrado != None: #Ustedes van a tener que cambiar esto, por una validación con la DB.
+            session['user_id'] = nombre #definición de cookie de sesión.
+            #return render_template('login.html', user=nombre)                
+            return json.dumps({'nombre': nombre, 'contrasenia': contrasenia})
+        flash('Invalid username or password')
+        #return redirect(url_for('login'))
+        return json.dumps({'error':'Invalid username or password'})
 
 
 @app.route('/logout')
